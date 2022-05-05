f3941ea6fe19a6bb151ff21e9fe530a722f0811c
lktslionel <lktslionel@gmail.com>
2022-05-05 02:21:43 +0200

    test: add a location example to test_helper

--- tests/test_helper.bash
+++ tests/test_helper.bash
@@ -6,6 +6,7 @@ export BATS_HELPERS_DIR="${HOME}/.bats/test_helper"
 
 export DEFAULT_SOURCE_LOCATION_WITHOUT_PATH="github.com:lktslionel/mock-repo"
 export DEFAULT_SOURCE_LOCATION_WITH_PATH="github.com:lktslionel/mock-repo:src"
+export LOCATION_WITH_UNKNOWN_PROVIDER="unknown:lktslionel/mock-repo:src"
 export DEFAULT_REPOSITORY="lktslionel/mock-repo"
 export DEFAULT_REPOSITORY_NAME="${DEFAULT_REPOSITORY#*/}"
 
